dbd0c21ad79173c2b184f00c0da5b47d85891030
niicsz <nicolasbbini@gmail.com>
2026-04-19 19:40:04 -0300

    feat: implement Redis caching for product management and token blacklist service

--- src/main/java/com/binitech/pdv/domain/Product.java
+++ src/main/java/com/binitech/pdv/domain/Product.java
@@ -1,8 +1,11 @@
 package com.binitech.pdv.domain;
 
+import java.io.Serializable;
 import java.util.Objects;
 
-public class Product {
+public class Product implements Serializable {
+
+  private static final long serialVersionUID = 1L;
 
   private String id;
   private String barcode;
