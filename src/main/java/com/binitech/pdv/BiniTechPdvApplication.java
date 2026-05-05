cbfdfa511b242d636340df9b42f50ffcfdd5eb42
niicsz <nicolasbbini@gmail.com>
2026-05-05 15:06:33 -0300

    fix: Redis cache configuration

--- src/main/java/com/binitech/pdv/BiniTechPdvApplication.java
+++ src/main/java/com/binitech/pdv/BiniTechPdvApplication.java
@@ -2,8 +2,11 @@ package com.binitech.pdv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = RedisRepositoriesAutoConfiguration.class)
+@EnableMongoRepositories(basePackages = "com.binitech.pdv.adapters.outbound.persistence.repository")
 public class BiniTechPdvApplication {
 
   public static void main(String[] args) {
