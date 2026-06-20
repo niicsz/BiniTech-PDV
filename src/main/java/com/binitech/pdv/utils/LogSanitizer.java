c872230b883bbe05112b340eb2187c3904af9431
niicsz <nicolasbbini@gmail.com>
2026-06-20 00:19:18 -0300

    feat: enhance logging and refactor tenant ID resolution in billing and login components

--- src/main/java/com/binitech/pdv/utils/LogSanitizer.java
+++ src/main/java/com/binitech/pdv/utils/LogSanitizer.java
@@ -4,28 +4,37 @@ public final class LogSanitizer {
 
   private LogSanitizer() {}
 
+  public static String neutralize(String value) {
+    if (value == null) {
+      return null;
+    }
+    return value.replaceAll("[\\p{Cntrl}]", "_");
+  }
+
   public static String maskUsername(String username) {
     if (username == null || username.isEmpty()) {
       return "***";
     }
-    int len = username.length();
+    String safe = neutralize(username);
+    int len = safe.length();
     if (len <= 2) {
       return "*".repeat(len);
     }
     if (len <= 4) {
-      return username.charAt(0) + "*".repeat(len - 1);
+      return safe.charAt(0) + "*".repeat(len - 1);
     }
-    return username.charAt(0) + "*".repeat(len - 2) + username.charAt(len - 1);
+    return safe.charAt(0) + "*".repeat(len - 2) + safe.charAt(len - 1);
   }
 
   public static String maskId(String id) {
     if (id == null || id.isEmpty()) {
       return "***";
     }
-    int len = id.length();
+    String safe = neutralize(id);
+    int len = safe.length();
     if (len <= 8) {
       return "*".repeat(len);
     }
-    return id.substring(0, 4) + "***" + id.substring(len - 4);
+    return safe.substring(0, 4) + "***" + safe.substring(len - 4);
   }
 }
